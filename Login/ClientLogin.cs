6306c2e01e5da64f231dddbdec53f8eaa3f44e13
pedroovilasboas <pedrovilasboas@hotmail.com>
2024-11-20 17:16:53 +0000

    More Login forms

---
+++ Login/ClientLogin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POO_25453_TP.Login
+{
+    public partial class ClientLogin : Form
+    {
+        public ClientLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void ClientLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
